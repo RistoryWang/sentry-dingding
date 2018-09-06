ad1c248935efc0d77d624cffbe31bd919b7727fa
ristory <ristory@gmail.com>
2018-09-06 14:29:00 +0800

    修改编码为utf8

--- sentry_dingding/models.py
+++ sentry_dingding/models.py
@@ -5,6 +5,9 @@ sentry_dingding.models
 :copyright: (c) 2011 by Linovia, see AUTHORS for more details.
 :license: BSD, see LICENSE for more details.
 """
+import sys
+reload(sys)
+sys.setdefaultencoding('utf8')
 import requests
 from django import forms
 from sentry.plugins.bases.notify import NotifyPlugin
